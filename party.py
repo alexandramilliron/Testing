13f2da6f7741b29bc6162e95ee614f3a3a3a3f14
alexandramilliron <alexandramilliron@gmail.com>
2020-10-19 13:54:37 -0700

    added tests to party.py and tests.py

--- party.py
+++ party.py
@@ -8,7 +8,13 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
+    >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+    True
+    >>> is_mel('Judith Butler', 'judith@awesome.com')
+    False
+    
+    """
 
     return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
 
@@ -17,6 +23,18 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+
+    >>> treats = [ 
+    ...    {'type': 'dessert'},
+    ...    {'type': 'dessert'},
+    ...    {'type': 'appetizer'},
+    ...    {'type': 'dessert'},
+    ...    {'type': 'appetizer'},
+    ...    {'type': 'drink'},
+    ... ]
+    >>> most_and_least_common_type(treats)
+    ('dessert', 'drink')
+
     """
 
     types = {}
@@ -43,8 +61,6 @@ def most_and_least_common_type(treats):
 
 def get_treats():
     """Return treats being brought to the party.
-
-    One day, I'll move this into a database! -- Balloonicorn
     """
 
     return [
